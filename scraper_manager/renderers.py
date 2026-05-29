4feb8a8c0c71fdba1d34934f5ccb0ffff7a3e8c5
expectexception <rajatrathee.main@gmail.com>
2026-05-29 18:08:49 +0530

    need testing

---
+++ scraper_manager/renderers.py
@@ -0,0 +1,19 @@
+import json
+from rest_framework.renderers import JSONRenderer
+from rest_framework.utils.encoders import JSONEncoder
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        # Handle bson ObjectId safely
+        if obj.__class__.__name__ == 'ObjectId':
+            return str(obj)
+        try:
+            from bson import ObjectId
+            if isinstance(obj, ObjectId):
+                return str(obj)
+        except ImportError:
+            pass
+        return super().default(obj)
+
+class CustomJSONRenderer(JSONRenderer):
+    encoder_class = MongoJSONEncoder
